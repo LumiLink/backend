795dbd070211aff26a888b562e19912627757f78
runevh <runevanhuffel@gmail.com>
2024-02-15 01:06:14 +0100

    Optimized for plugin loading

--- src/main/java/net/lumilink/api/devices/DeviceData.java
+++ src/main/java/net/lumilink/api/devices/DeviceData.java
@@ -3,5 +3,23 @@ package net.lumilink.api.devices;
 import java.util.List;
 
 public class DeviceData {
-    private List<Object> data;
+    private final List<Object> data;
+
+    public DeviceData(Object... data) {
+        this.data = List.of(data);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("<Device Data: {");
+        for(int i = 0; i < data.size(); i++){
+            s.append(data.get(i).toString());
+            if(i < data.size() - 1){
+                s.append(", ");
+            }
+        }
+
+        s.append(">}");
+        return s.toString();
+    }
 }
---
+++ src/main/java/net/lumilink/api/lib/Server.java
@@ -0,0 +1,6 @@
+package net.lumilink.api.lib;
+
+public interface Server {
+
+    void log(String s);
+}
